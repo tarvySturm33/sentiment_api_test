e8dc90d64ab9a0f6c7270189854d171d83ee80eb
JonasALONSO <joebabes-dev@proton.me>
2026-05-25 15:17:09 +0000

    add authorization tests

---
+++ tests/test_authorization.py
@@ -0,0 +1,89 @@
+from dotenv import load_dotenv
+from datetime import datetime as dt
+import os
+
+from RequestBuilder import RequestBuilder
+
+# date format
+dt_ft = '%Y-%m-%d %H:%M:%S.%f'
+
+# load variables of the target environment (test)
+
+load_dotenv()
+
+api_host = os.environ.get('HOST', '0.0.0.0')
+api_port = os.environ.get('PORT', 8000)
+
+# instantiate request builder
+
+request_builder = RequestBuilder(
+    host=api_host,
+    port=api_port
+)
+
+# test dataset
+test_data = [
+    {
+        'username': 'alice',
+        'password': 'wonderland',
+        'entries': [
+            {'target': '/v1', 'expected_code': 200},
+            {'target': '/v2', 'expected_code': 200}
+            ]
+    },
+    {
+        'username': 'bob',
+        'password': 'builder',
+        'entries': [
+            {'target': '/v1', 'expected_code': 200},
+            {'target': '/v2', 'expected_code': 403}
+            ]
+    }
+]
+
+# endpoint to test
+endpoint = '{}/sentiment'
+
+for user in test_data:
+    for entry in user['entries']:
+        target = entry['target']
+        response = request_builder.get(
+            endpoint=endpoint.format(target),
+            params={
+                'username': user['username'],
+                'password': user['password']
+            }
+        )
+        output = '''
+        ============================
+            Authorization test
+        ============================
+
+        request done to "{endpoint}" at {timestamp}
+
+        | username= {usr}
+        | password= {pwd}
+        expected result = {expected_code}
+        actual result = {status_code}
+        ==>  {test_status}
+        '''
+        # query status
+        status_code = response.status_code
+        # display the results
+        if status_code == entry['expected_code']:
+            test_status = 'SUCCESS'
+        else:
+            test_status = 'FAILURE'
+        output = output.format(status_code=status_code,
+                               test_status=test_status,
+                               expected_code=entry['expected_code'],
+                               usr=user['username'],
+                               pwd=user['password'],
+                               endpoint=endpoint.format(target),
+                               timestamp=dt.now().strftime(dt_ft)[:-3])
+        # printing in a file
+        if os.environ.get('LOG') == '1':
+            with open('api_test.log', 'a') as file:
+                file.write(output)
+        else:
+            print(output)

dfec871193ee349b270296e724a5affaf14bb80e
JonasALONSO <joebabes-dev@proton.me>
2026-05-25 13:34:29 +0000

    refactor tests with RequestBuilder class

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,36 +1,63 @@
 from dotenv import load_dotenv
+from datetime import datetime as dt
 import os
-import requests
-# definition of the API's params
+
+from RequestBuilder import RequestBuilder
+
+# date format
+dt_ft = '%Y-%m-%d %H:%M:%S.%f'
+
+# load variables of the target environment (test)
 
 load_dotenv()
+
 api_host = os.environ.get('HOST', '0.0.0.0')
 api_port = os.environ.get('PORT', 8000)
 
+# instantiate request builder
+
+request_builder = RequestBuilder(
+    host=api_host,
+    port=api_port
+)
+
+# test dataset
 test_data = [
-    {'username': 'alice',
-     'password': 'wonderland',
-     'expected_code': 200},
-    {'username': 'bob',
-     'password': 'builder',
-     'expected_code': 200},
-    {'username': 'clementine',
-     'password': 'mandarine',
-     'expected_code': 403}
-    ]
+    {
+        'username': 'alice',
+        'password': 'wonderland',
+        'expected_code': 200
+    },
+    {
+        'username': 'bob',
+        'password': 'builder',
+        'expected_code': 200
+    },
+    {
+        'username': 'clementine',
+        'password': 'mandarine',
+        'expected_code': 403
+    }
+]
+
+# endpoint to test
+endpoint = '/permissions'
 
 for user in test_data:
-    r = requests.get(url='http://{address}:{port}/permissions'.
-                     format(address=api_host, port=api_port),
-                     params={
-                         'username': user['username'],
-                         'password': user['password']
-                         })
+    response = request_builder.get(
+        endpoint=endpoint,
+        params={
+            'username': user['username'],
+            'password': user['password']
+        }
+    )
     output = '''
     ============================
         Authentication test
     ============================
-    request done at "/permissions"
+
+    request done to "{endpoint}" at {timestamp}
+
     | username= {usr}
     | password= {pwd}
     expected result = {expected_code}
@@ -38,7 +65,7 @@ for user in test_data:
     ==>  {test_status}
     '''
     # query status
-    status_code = r.status_code
+    status_code = response.status_code
     # display the results
     if status_code == user['expected_code']:
         test_status = 'SUCCESS'
@@ -48,7 +75,9 @@ for user in test_data:
                            test_status=test_status,
                            expected_code=user['expected_code'],
                            usr=user['username'],
-                           pwd=user['password'])
+                           pwd=user['password'],
+                           endpoint=endpoint,
+                           timestamp=dt.now().strftime(dt_ft)[:-3])
     # printing in a file
     if os.environ.get('LOG') == '1':
         with open('api_test.log', 'a') as file:
